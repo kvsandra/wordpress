b8d47cc36b842650b8c54045205cc8b3371726bf
kvsandra <142771413+kvsandra@users.noreply.github.com>
2023-09-05 09:24:13 +0530

    Add files via upload

--- feedback-manager.php
+++ feedback-manager.php
@@ -1,96 +1,101 @@
 <?php
-/**
- * Plugin Name: Feedback Manager
- * Description: A plugin to manage feedback.
- * Version: 1.0.0
- * Author: Your Name
- */
+/*
+Plugin Name: Feedback Manager
+Description: Plugin for managing user feedback
+Version: 1.0
+Author: Your Name
+*/
 
-// Enqueue CSS and JS files
-function feedback_manager_enqueue_scripts() {
-    wp_enqueue_style( 'feedback-manager-style', plugins_url( 'css/style.css', __FILE__ ) );
-    wp_enqueue_script( 'feedback-manager-script', plugins_url( 'js/script.js', __FILE__ ), array( 'jquery' ), '1.0.0', true );
+// Plugin activation hook
+register_activation_hook( __FILE__, 'feedback_manager_activate' );
+
+function feedback_manager_activate() {
+    // Add any activation tasks here
+    feedback_manager_create_table();
 }
-add_action( 'wp_enqueue_scripts', 'feedback_manager_enqueue_scripts' );
 
-// Display feedback form at the bottom of posts and pages
-function feedback_manager_display_form() {
-    if ( is_singular() ) {
-        $form_html = '
-        <div class="feedback-form">
-            <h3>Leave Feedback</h3>
-            <form id="feedback-form" method="post" action="http://localhost:8888/wp/2023/09/04/thank-you/">
-                <div class="form-group">
-                    <label for="user_name">Name</label>
-                    <input type="text" name="user_name" id="user_name" required>
-                </div>
-                <div class="form-group">
-                    <label for="user_email">Email</label>
-                    <input type="email" name="user_email" id="user_email" required>
-                </div>
-                <div class="form-group">
-                    <label for="feedback_type">Feedback Type</label>
-                    <select name="feedback_type" id="feedback_type" required>
-                        <option value="General">General</option>
-                        <option value="Bug Report">Bug Report</option>
-                        <option value="Feature Request">Feature Request</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="feedback_message">Message</label>
-                    <textarea name="feedback_message" id="feedback_message" required></textarea>
-                </div>
-                <input type="submit" value="Submit Feedback">
-            </form>
-        </div>';
+// Plugin deactivation hook
+register_deactivation_hook( __FILE__, 'feedback_manager_deactivate' );
 
-        echo $form_html;
+function feedback_manager_deactivate() {
+    // Add any deactivation tasks here
+}
+
+// Add feedback form at the bottom of posts and pages
+add_filter( 'the_content', 'feedback_manager_display_form' );
+
+
+
+
+
+
+function feedback_manager_display_form( $content ) {
+    if ( is_singular( array( 'post', 'page' ) ) ) {
+        ob_start();
+
+        global $post;
+        ?>
+        <form id="feedback-form" method="post" action="http://localhost:8888/wp/2023/09/04/thank-you/">
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" required>
+            <input type="hidden" name="post_id" value="<?php echo esc_attr($post->ID); ?>">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" required>
+
+            <label for="feedback-type">Feedback Type</label>
+            <select name="feedback-type" id="feedback-type" required>
+                <option value="">Select feedback type</option>
+                <option value="Bug Report">Bug Report</option>
+                <option value="Feature Request">Feature Request</option>
+                <option value="General Feedback">General Feedback</option>
+            </select>
+
+            <label for="message">Message</label>
+            <textarea name="message" id="message" required></textarea>
+
+            <input type="submit" value="Submit" name="submit_form">
+        </form>
+        <?php
+        $form = ob_get_clean();
+        $content .= $form;
+    }
+    return $content;
+}
+
+// Save submitted feedback to database
+add_action( 'wp_loaded', 'feedback_manager_save_feedback' );
+
+function feedback_manager_save_feedback() {
+    if ( isset( $_POST['name'] ) && isset( $_POST['email'] ) && isset( $_POST['feedback-type'] ) && isset( $_POST['message'] ) ) {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'feedback';
+
+        $data = array(
+            'post_id' => absint( $_POST['post_id'] ),
+            'name' => sanitize_text_field( $_POST['name'] ),
+            'email' => sanitize_email( $_POST['email'] ),
+            'feedback_type' => sanitize_text_field( $_POST['feedback-type'] ),
+            'message' => sanitize_textarea_field( $_POST['message'] ),
+            'submission_timestamp' => current_time( 'mysql' ),
+        );
+
+        $wpdb->insert( $table_name, $data );
     }
 }
-add_action( 'the_content', 'feedback_manager_display_form' );
 
-
-
-function feedback_manager_process_form() {
-    if ( isset( $_POST['user_name'] ) && isset( $_POST['user_email'] ) && isset( $_POST['feedback_type'] ) && isset( $_POST['feedback_message'] ) ) {
-        $user_name = sanitize_text_field( $_POST['user_name'] );
-        $user_email = sanitize_email( $_POST['user_email'] );
-        $feedback_type = sanitize_text_field( $_POST['feedback_type'] );
-        $feedback_message = sanitize_textarea_field( $_POST['feedback_message'] );
-
-        // Validate email
-        if ( ! is_email( $user_email ) ) {
-            // Handle invalid email error
-            wp_die( 'Invalid email address.' );
-        }
-
-        // Store feedback in the database or perform other actions
-        store_feedback( get_the_ID(), $user_name, $user_email, $feedback_type, $feedback_message );
-
-        // Redirect or display success message
-        wp_redirect( get_permalink() . '?feedback=submitted' );
-        exit;
-    }
-}
-add_action( 'init', 'feedback_manager_process_form' );
-
-
-
-
-function create_feedback_table() {
+// Create custom database table on plugin activation
+function feedback_manager_create_table() {
     global $wpdb;
-
     $table_name = $wpdb->prefix . 'feedback';
-
     $charset_collate = $wpdb->get_charset_collate();
 
     $sql = "CREATE TABLE $table_name (
         id mediumint(9) NOT NULL AUTO_INCREMENT,
-        post_id int(11) NOT NULL,
-        user_name varchar(255) NOT NULL,
-        user_email varchar(255) NOT NULL,
+        post_id bigint(20) NOT NULL,
+        name varchar(255) NOT NULL,
+        email varchar(255) NOT NULL,
         feedback_type varchar(255) NOT NULL,
-        feedback_message text NOT NULL,
+        message text NOT NULL,
         submission_timestamp datetime NOT NULL,
         PRIMARY KEY (id)
     ) $charset_collate;";
@@ -98,114 +103,70 @@ function create_feedback_table() {
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
     dbDelta( $sql );
 }
-add_action( 'after_setup_theme', 'create_feedback_table' );
 
+// Create admin page for managing feedback
+add_action( 'admin_menu', 'feedback_manager_admin_menu' );
 
-
-
-function store_feedback( $post_id, $user_name, $user_email, $feedback_type, $feedback_message ) {
-    global $wpdb;
-
-    $table_name = $wpdb->prefix . 'feedback';
-
-    $wpdb->insert(
-        $table_name,
-        array(
-            'post_id' => $post_id,
-            'user_name' => $user_name,
-            'user_email' => $user_email,
-            'feedback_type' => $feedback_type,
-            'feedback_message' => $feedback_message,
-            'submission_timestamp' => current_time( 'mysql' ),
-        )
-    );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function add_feedback_admin_page() {
+function feedback_manager_admin_menu() {
     add_menu_page(
-        'Feedback Admin Page', // Page title
-        'Feedback', // Menu title
-        'manage_options', // Capability required to access the page
-        'feedback-admin', // Menu slug
-        'display_feedback_admin_page', // Callback function to display the page content
-        'dashicons-feedback', // Menu icon
-        25 // Position in the admin menu
+        'Feedback Manager',
+        'Feedback Manager',
+        'manage_options',
+        'feedback-manager',
+        'feedback_manager_admin_page',
+        'dashicons-feedback',
+        20
     );
 }
-add_action('admin_menu', 'add_feedback_admin_page');
 
-// Callback function to display the admin page content
+function feedback_manager_admin_page() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'feedback';
+    $per_page = 10;
+    $current_page = isset( $_GET['paged'] ) ? absint( $_GET['paged'] ) : 1;
+    $total_items = $wpdb->get_var( "SELECT COUNT(*) FROM $table_name" );
+    $total_pages = ceil( $total_items / $per_page );
 
-
-
-function display_feedback_admin_page() {
+    $feedback = $wpdb->get_results(
+        "SELECT * FROM $table_name ORDER BY submission_timestamp DESC LIMIT " . ( $current_page - 1 ) * $per_page . ", $per_page"
+    );
     ?>
     <div class="wrap">
-        <h1>Feedback Admin Page</h1>
+        <h1>Feedback Manager</h1>
         <table class="wp-list-table widefat fixed striped">
             <thead>
                 <tr>
-                    <th>Post/Page</th>
-                    <th>Feedback Type</th>
-                    <th>Timestamp</th>
+                    <th scope="col">Post/Page</th>
+                    <th scope="col">Feedback Type</th>
+                    <th scope="col">Timestamp</th>
                 </tr>
             </thead>
             <tbody>
-                <?php
-                // Query to retrieve feedback data
-                $feedback_args = array(
-                    'post_type' => 'feedback', // Replace with your custom post type name
-                    'posts_per_page' => -1
-                );
-                $feedback_query = new WP_Query($feedback_args);
-
-                // Loop through the feedback and display in table rows
-                if ($feedback_query->have_posts()) :
-                    while ($feedback_query->have_posts()) : $feedback_query->the_post();
-                        $post_title = get_the_title();
-                        $feedback_type = get_post_meta(get_the_ID(), 'feedback_type', true);
-                        $timestamp = get_the_time('Y-m-d H:i:s');
-                        ?>
-                        <tr>
-                            <td><?php echo $post_title; ?></td>
-                            <td><?php echo $feedback_type; ?></td>
-                            <td><?php echo $timestamp; ?></td>
-                        </tr>
-                        <?php
-                        // Send email notification to the site administrator
-                        $to = get_option('admin_email');
-                        $subject = 'New Feedback Submission';
-                        $message = "A new feedback has been submitted:\n\n";
-                        $message .= "Post/Page: $post_title\n";
-                        $message .= "Feedback Type: $feedback_type\n";
-                        $message .= "User's Email: " . get_post_meta(get_the_ID(), 'user_email', true) . "\n";
-                        wp_mail($to, $subject, $message);
-                    endwhile;
-                    wp_reset_postdata();
-                else :
-                    ?>
+                <?php foreach ( $feedback as $item ) : ?>
                     <tr>
-                        <td colspan="3">No feedback found.</td>
+                        <td><?php echo get_the_title( $item->post_id ); ?></td>
+                        <td><?php echo $item->feedback_type; ?></td>
+                        <td><?php echo $item->submission_timestamp; ?></td>
                     </tr>
-                    <?php
-                endif;
-                ?>
+                <?php endforeach; ?>
             </tbody>
         </table>
+        <?php
+        $page_links = paginate_links(
+            array(
+                'base' => add_query_arg( 'paged', '%#%' ),
+                'format' => '',
+                'prev_text' => '&laquo;',
+                'next_text' => '&raquo;',
+                'total' => $total_pages,
+                'current' => $current_page,
+            )
+        );
+
+        if ( $page_links ) {
+            echo '<div class="tablenav"><div class="tablenav-pages">' . $page_links . '</div></div>';
+        }
+        ?>
     </div>
     <?php
 }
@@ -213,29 +174,20 @@ function display_feedback_admin_page() {
 
 
 
-function send_feedback_notification( $post_id, $user_email, $feedback_type ) {
-    $post_title = get_the_title( $post_id );
+function form_capture()
+{
+    if(array_key_exists('submit_form',$_POST))
+    {
+        $to = get_option( 'sandrakv@mozilor.com' );
+        $post_title = get_the_title( $post_id );
+        $subject = 'New Feedback Submission';
+        $body = '';
 
-    $to = get_option( 'admin_email' );
-    $subject = 'New Feedback Submission';
-    $message = "A new feedback has been submitted:\n\n";
-    $message .= "Post/Page: $post_title\n";
-    $message .= "Feedback Type: $feedback_type\n";
-    $message .= "User Email: $user_email";
+       $body .= 'feedback_type:'.$_POST('feedback-type').'<br /> ';
+       $body .= 'email:'.$_POST('email').'<br /> ';
+        
+        wp_mail($to,$subject,$body);
 
-    wp_mail( $to, $subject, $message );
+    }
 }
-
-
-
-function submit_feedback( $post_id, $user_name, $user_email, $feedback_type, $feedback_message ) {
-    // ...
-    $wpdb->insert(
-        $table_name,
-        array(
-            // ...
-        )
-    );
-
-    send_feedback_notification( $post_id, $user_email, $feedback_type );
-}
+add_action('wp_head','form_capture');
